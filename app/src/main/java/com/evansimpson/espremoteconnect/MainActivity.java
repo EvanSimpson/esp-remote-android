a1f355df18bc5db615b547a2fcab95db986ed429
Evan Simpson <me@evansimpson.com>
2017-03-03 13:04:55 -0500

    Add blue scanning functionality

--- app/src/main/java/com/evansimpson/espremoteconnect/MainActivity.java
+++ app/src/main/java/com/evansimpson/espremoteconnect/MainActivity.java
@@ -1,13 +1,149 @@
 package com.evansimpson.espremoteconnect;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_ENABLE_LOCATION = 2;
+    private static final long SCAN_PERIOD = 10000;
+
+    private boolean mScanning = false;
+    private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler;
+    private BluetoothDevice mRemoteDevice;
+    private String deviceName = "ESP_REMOTE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mHandler = new Handler();
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            this.requestPermissions(
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_ENABLE_LOCATION);
+
+        } else {
+            requestBle();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_ENABLE_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestBle();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+        scanLeDevice(true);
+    }
+
+    protected void requestBle() {
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            scanLeDevice(true);
+        }
+
+    }
+
+    private void updateConnectionLabel(boolean found) {
+        TextView connectionLabel = (TextView) findViewById(R.id.connection_label);
+        if (mScanning) {
+            connectionLabel.setText(R.string.remote_scanning);
+        } else {
+            if (found) {
+                connectionLabel.setText(R.string.remote_connected);
+            } else {
+                connectionLabel.setText(R.string.remote_disconnected);
+            }
+        }
+    }
+
+    protected void scanLeDevice(final boolean enable) {
+        if (enable) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mScanning) {
+                        mScanning = false;
+                        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                        updateConnectionLabel(false);
+                    }
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+            updateConnectionLabel(false);
+        } else {
+            if (mScanning) {
+                mScanning = false;
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            }
+        }
+    };
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+            if (device.getName() != null && device.getName().equals(deviceName)) {
+                Log.d("Device status", "device found");
+                mRemoteDevice = device;
+                scanLeDevice(false);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateConnectionLabel(true);
+                    }
+                });
+            }
+        }
+    };
 }
