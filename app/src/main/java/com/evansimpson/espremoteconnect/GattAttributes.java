739d49861269062a8f715e71837e39757db6d30c
Evan Simpson <me@evansimpson.com>
2017-03-03 16:48:50 -0500

    Add GATT attribute constants

---
+++ app/src/main/java/com/evansimpson/espremoteconnect/GattAttributes.java
@@ -0,0 +1,10 @@
+package com.evansimpson.espremoteconnect;
+
+/**
+ * Created by evan on 3/3/17.
+ */
+
+public class GattAttributes {
+    public static String REMOTE_SERVICE_UUID = "CFFBCCBA-E23B-4E6C-B5D8-73D97BFF0000";
+    public static String CODE_CHAR_UUID = "CFFBCCBA-E23B-4E6C-B5D8-73D97BFF1000";
+}
