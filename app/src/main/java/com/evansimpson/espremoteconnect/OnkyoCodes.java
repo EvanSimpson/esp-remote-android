fe4fb81e82f6b1bdba4009d94fe4052cbb4940e2
Evan Simpson <me@evansimpson.com>
2017-03-03 17:55:12 -0500

    Add test IR code

---
+++ app/src/main/java/com/evansimpson/espremoteconnect/OnkyoCodes.java
@@ -0,0 +1,9 @@
+package com.evansimpson.espremoteconnect;
+
+/**
+ * Created by evan on 3/3/17.
+ */
+
+public class OnkyoCodes {
+    public static byte ONKYO_KEY_POWER[] = {(byte) 0xB6, (byte) 0x20, (byte) 0xDF};
+}
